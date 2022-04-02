329e57fe98164fca3bebec73770272ab111c46c4
Saccha-create <fukushima.satsuki@kindai.ac.jp>
2022-04-02 21:10:14 +0900

    テスト再掲

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  # devise_for :users
+  # resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 get 'students/index' => 'students#index'
 get 'students/new' => 'students#new'
+post 'students' => 'students#create'
 end
